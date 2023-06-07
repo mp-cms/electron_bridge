9ee32680aae8f16ef9157df466e7fc640fe76e93
Martin Pimon <martin.pimon@tuwien.ac.at>
2023-06-07 10:24:25 +0200

    First implementation of T_M1

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ Angular parts can be analytically solved,  radial parts are integrated.
 The photon multipolarity is dominantly E1,
 while the electronic and nuclear transition is a mixture of E2 and M1
 see 10.1103/PhysRevA.103.053120
+TODO: Conceptually mixed real and complex spherical harmonics
+TODO: More k-points
 '''
 import yaml
 import numpy as np
 
 from tqdm import tqdm
 # import matplotlib.pyplot as plt
-# from time import time
+from time import time
 
+from sympy import KroneckerDelta
 from sympy.physics.wigner import real_gaunt
+from scipy.constants import speed_of_light as c0
 
 
 def read_grid(fname='r_grid.yaml'):
@@ -21,7 +25,7 @@ def read_grid(fname='r_grid.yaml'):
     Read the radial grid
     '''
     with open(fname, "r", encoding="utf-8") as stream:
-        return yaml.safe_load(stream)
+        return yaml.load(stream, Loader=yaml.CSafeLoader)
 
 
 def read_alm(band):
@@ -32,7 +36,7 @@ def read_alm(band):
     lmax = 12
     fname = f"{band}_{lmax}.yaml"
     with open(fname, "r", encoding="utf-8") as stream:
-        data = yaml.safe_load(stream)
+        data = yaml.load(stream, Loader=yaml.CSafeLoader)
     # data loads as list of dicts but the keys and values are just strings
     alm_band = [{(l_q, m_q): complex(0., 0.)
                  for l_q in range(0, lmax+1) for m_q in range(-l_q, l_q+1)}
@@ -79,21 +83,46 @@ def trapezoidal_integration(integrand, grid):
     return integral
 
 
+def l_plus(l_q, m_q):
+    '''
+    Application of l_+ Y_lm
+    The spherical harmonic Y_lm+1 is omitted
+    '''
+    return -1/np.sqrt(2) * np.sqrt((l_q - m_q) * (l_q + m_q + 1))
+
+
+def l_minus(l_q, m_q):
+    '''
+    Application of l_- Y_lm
+    The spherical harmonic Y_lm-1 is omitted
+    '''
+    return 1/np.sqrt(2) * np.sqrt(l_q*(l_q + 1) - m_q*(m_q - 1))
+
+
+def l_0(m_q):
+    '''
+    Application of l_0 Y_lm
+    The spherical harmonic Y_lm is omitted
+    '''
+    return m_q
+
+
 def f_qe1_i(alm_final, alm_initial, grid, transition_m):
     '''
     Calculates <f|Q_E1|i>, where Q_EI = -r
     '''
+    assert -1 <= transition_m <= 1
     sum_lm = complex(0., 0.)
-    for (l1_q, m1_q) in tqdm(alm_initial[0].keys()):
-        for (l2_q, m2_q) in alm_final[0].keys():
-            angular_part = real_gaunt(l1_q, l2_q, 1, m1_q, m2_q, transition_m,
+    for (li_q, mi_q) in tqdm(alm_initial[0].keys()):
+        for (lf_q, mf_q) in alm_final[0].keys():
+            angular_part = real_gaunt(li_q, lf_q, 1, mi_q, mf_q, transition_m,
                                       prec=16)
             if angular_part < 1e-10:
                 continue
-            integrand = [np.conj(alm_rf[(l2_q, m2_q)]) * alm_ri[(l1_q, m1_q)] *
-                         grid[grid_i]**3
-                         for grid_i, (alm_ri, alm_rf)
-                         in enumerate(zip(alm_initial, alm_final))]
+            integrand = [np.conj(alm_rf[(lf_q, mf_q)]) * alm_ri[(li_q, mi_q)] *
+                         grid_r**3
+                         for grid_r, alm_ri, alm_rf
+                         in zip(grid, alm_initial, alm_final)]
             radial_part = trapezoidal_integration(integrand, grid)
             sum_lm += radial_part * angular_part
     return np.sqrt(4.*np.pi/3.) * sum_lm
@@ -103,20 +132,64 @@ def f_te2_i(alm_final, alm_initial, grid, transition_m):
     '''
     Calculates <f|T_E2|i>, where T_E2 = -1/r³ sqrt(4π/5)Y_{2,q}
     '''
+    assert -2 <= transition_m <= 2
     sum_lm = complex(0., 0.)
-    for (l1_q, m1_q) in tqdm(alm_initial[0].keys()):
-        for (l2_q, m2_q) in alm_final[0].keys():
-            angular_part = real_gaunt(l1_q, l2_q, 2, m1_q, m2_q, transition_m,
+    for (li_q, mi_q) in tqdm(alm_initial[0].keys()):
+        for (lf_q, mf_q) in alm_final[0].keys():
+            angular_part = real_gaunt(li_q, lf_q, 2, mi_q, mf_q, transition_m,
                                       prec=16)
             if angular_part < 1e-10:
                 continue
-            integrand = [np.conj(alm_rf[(l2_q, m2_q)]) * alm_ri[(l1_q, m1_q)] *
-                         1./grid[grid_i]
-                         for grid_i, (alm_ri, alm_rf)
-                         in enumerate(zip(alm_initial, alm_final))]
+            integrand = [np.conj(alm_rf[(lf_q, mf_q)]) * alm_ri[(li_q, mi_q)] *
+                         1./grid_r
+                         for grid_r, alm_ri, alm_rf
+                         in zip(grid, alm_initial, alm_final)]
             radial_part = trapezoidal_integration(integrand, grid)
             sum_lm += radial_part * angular_part
     return np.sqrt(4.*np.pi/5.) * sum_lm
+
+
+def f_tm1_i(alm_final, alm_initial, grid, transition_m):
+    '''
+    Calculates <f|T_M1|i>,
+    where T_M1 = 1/c[l/r³ - σ/(2r³) + 3r(r·σ)/2r⁵ + 4πσδ(r)/3]
+    '''
+    assert -1 <= transition_m <= 1
+    factor = np.sqrt(3./(20.*np.pi))
+    m_s = 1./2.
+    sum_lm = complex(0., 0.)
+    for (li_q, mi_q) in tqdm(alm_initial[0].keys()):
+        for (lf_q, mf_q) in alm_final[0].keys():
+            if transition_m == 1:
+                angular_part = -1/np.sqrt(2) * l_plus(li_q, mi_q) *\
+                               KroneckerDelta(lf_q, li_q) *\
+                               KroneckerDelta(mf_q, mi_q+1) +\
+                               real_gaunt(lf_q, 2, li_q, mf_q, 1, mi_q,
+                                          prec=16) *\
+                               4*np.pi*factor*m_s
+            elif transition_m == -1:
+                angular_part = -1/np.sqrt(2) * l_minus(li_q, mi_q) *\
+                               KroneckerDelta(lf_q, li_q) *\
+                               KroneckerDelta(mf_q, mi_q-1) +\
+                               real_gaunt(lf_q, 2, li_q, mf_q, -1, mi_q,
+                                          prec=16) *\
+                               4*np.pi*factor*m_s
+            elif transition_m == 0:
+                angular_part = (1./(4.*np.pi) * l_0(mi_q)) *\
+                               KroneckerDelta(lf_q, li_q) *\
+                               KroneckerDelta(mf_q, mi_q) +\
+                               real_gaunt(lf_q, 2, li_q, mf_q, 0, mi_q,
+                                          prec=16) *\
+                               4*np.pi*factor*m_s/np.sqrt(5.*np.pi)
+            if angular_part < 1e-10:
+                continue
+            integrand = [np.conj(alm_rf[(lf_q, mf_q)]) * alm_ri[(li_q, mi_q)] *
+                         1./grid_r
+                         for grid_r, alm_ri, alm_rf
+                         in zip(grid, alm_initial, alm_final)]
+            radial_part = trapezoidal_integration(integrand, grid)
+            sum_lm += radial_part * angular_part
+    return 1./c0 * sum_lm
 
 
 if __name__ == "__main__":
@@ -129,7 +202,7 @@ if __name__ == "__main__":
     BAND_MAX = 241
     ALM_INITIAL = extrapolate_nucleus(read_alm(BAND_INITIAL), GRID)
     ALM_FINAL = extrapolate_nucleus(read_alm(BAND_FINAL), GRID)
-    print(f_qe1_i(ALM_FINAL, ALM_INITIAL, GRID, 0))
+    print(f_tm1_i(ALM_FINAL, ALM_INITIAL, GRID, 0))
     assert 0
     ALM_INTERMEDIATE = [extrapolate_nucleus(read_alm(BAND), GRID)
                         for BAND in range(BAND_MIN, BAND_MAX+1)]
